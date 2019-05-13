00c91efc0a9f07598741a0509b2750a0ccc5a940
Citach <l.acheron01@live.fr>
2019-05-13 11:26:24 +0200

    ll

--- src/main/java/com/adaming/entities/Role.java
+++ src/main/java/com/adaming/entities/Role.java
@@ -1,5 +1,40 @@
 package com.adaming.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Role {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long idRole;
+private String nomRole;
+public Role(Long idRole, String nomRole) {
+	super();
+	this.idRole = idRole;
+	this.nomRole = nomRole;
+}
+public Role() {
+	super();
+}
+public Role(String nomRole) {
+	super();
+	this.nomRole = nomRole;
+}
+public Long getIdRole() {
+	return idRole;
+}
+public void setIdRole(Long idRole) {
+	this.idRole = idRole;
+}
+public String getNomRole() {
+	return nomRole;
+}
+public void setNomRole(String nomRole) {
+	this.nomRole = nomRole;
+}
+
 
 }
